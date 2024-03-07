768bed9c721d4306ec3830628fbf60b699bdec44
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2024-03-07 13:33:59 +0100

    header fixes

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,15 +4,16 @@ const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
   return (
     <div className="movie" key={imdbID}>
       <div>
-        <p>{Year}</p>
+        
       </div>
 
       <div>
-        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        <img src={Poster !== "N/A" ? Poster : "https://dummyimage.com/300x400/fff/aaa"} alt={Title} />
       </div>
 
       <div>
         <span>{Type}</span>
+        <p>{Year}</p>
         <h3>{Title}</h3>
       </div>
     </div>
